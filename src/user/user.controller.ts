3f3e869def40bc53a8d9a232d2444a31e8c18465
Shin☘ <huutrimega@gmail.com>
2021-02-07 01:21:44 +0700

    update user model

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { LoginResult } from './interfaces/loginResult.interface';
+import { RegisterDto } from './dto/register.dto';
 
 @ApiTags('USER')
 @Controller('user')
@@ -15,5 +18,15 @@ export class UserController {
   @Get('secret')
   GetSecret(@Req() req): string {
     return this.userService.Secret(req.user.id);
+  }
+
+  @Post('login')
+  async Login(@Body() loginData: LoginDto): Promise<LoginResult> {
+    return this.userService.Login(loginData);
+  }
+
+  @Post('register')
+  async Register(@Body() registerData: RegisterDto): Promise<boolean> {
+    return this.userService.Register(registerData);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,15 @@
+import { User, UserDocument } from 'src/models/user.schema';
 import { LoginResult } from './interfaces/loginResult.interface';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   Public(): string {
     return 'public';
   }
@@ -11,5 +18,38 @@ export class UserService {
     return `secret: ${userId}`;
   }
 
-  // Login(loginData: LoginDto): Promise<LoginResult> {}
+  async Login(loginData: LoginDto): Promise<LoginResult> {
+    const user = await this.userModel.findOne({ username: loginData.username });
+    if (await this.userModel['comparePassword'](user, loginData.password))
+      return { token: 'ok' };
+    else
+      throw new HttpException(
+        'Username or password incorrect',
+        HttpStatus.BAD_REQUEST,
+      );
+  }
+
+  async Register(registerDto: RegisterDto): Promise<boolean> {
+    const user = await this.userModel.findOne({
+      username: registerDto.username,
+    });
+
+    if (user)
+      throw new HttpException(
+        'Username already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    else {
+      try {
+        const newUser: User = {
+          name: registerDto.name,
+          username: registerDto.username,
+          password: registerDto.password,
+        };
+        await new this.userModel(newUser).save();
+      } catch {
+        return false;
+      }
+    }
+  }
 }
